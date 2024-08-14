484decc5c7afea4f906ed10767e11a4630506cc6
Anna <m.a.s.1126@yandex.ru>
2024-08-14 22:33:03 +0300

    HW L12

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,70 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-    Shape circle = new Circle(7.0, "Серый", "Синий");
-    Shape rectangle = new Rectangle(5.0, 10.0, "Желтый", "Зеленый");
-    Shape triangle = new Triangle(8.0, 7.0, 12.0, "Коричневый", "Красный");
 
-        circle.displayCharacteristics();
-        rectangle.displayCharacteristics();
-        triangle.displayCharacteristics();
+        String[][] correctArr = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"},
+        };
+
+        String[][] incorrectSizeArr = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"},
+        };
+
+        String[][] incorrectDataArr = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"тринадцать", "14", "15", "16"}
+        };
+
+        try {
+            int sum = checkArray(correctArr);
+            System.out.println("Сумма элементов массива = " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            int sum = checkArray(incorrectSizeArr);
+            System.out.println("Сумма элементов массива = " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            int sum = checkArray(incorrectDataArr);
+            System.out.println("Сумма элементов массива = " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
+    public static int checkArray(String[][] array)
+            throws MyArraySizeException, MyArrayDataException {
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array.length != 4 || array[i].length != 4) {
+                    throw new MyArraySizeException("Размер массива должен быть 4x4, найден " + array.length + "x" + array[i].length);
+                }else {
+
+                    try {
+                        sum += Integer.parseInt(array[i][j]);
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "] - значение '" + array[i][j] + "'");
+                    }
+                }
+            }
+        }
+
+        return sum;
     }
 }
